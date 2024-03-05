28c0636eeb778791c8313c9490aa892ccc3ab942
OmarSuarezDoro <alu0101483474@ull.edu.es>
2024-03-05 13:49:02 +0000

    Updates

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { A } from './clase.js'
+
+let a = new A;
+console.log(a);
---
+++ tests/test.spec.ts
@@ -0,0 +1,14 @@
+import "mocha";
+import { expect } from "chai";
+
+describe("add function tests", () => {
+  it("expect to fail", () => {
+    expect(true).to.be.true;
+  });
+  //   expect(add(1, 8)).to.be.equal(9);
+  // });
+
+  // it("add(-1, 8) returns value 7", () => {
+  //   expect(add(-1, 8)).to.be.equal(7);
+  // });
+});
