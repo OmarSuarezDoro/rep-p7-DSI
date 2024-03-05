9bd25818c2c7b1611c047d8733f4f1bc28ef2761
OmarSuarezDoro <alu0101483474@ull.edu.es>
2024-03-05 18:58:07 +0000

    Prueba

--- src/clase.ts
+++ src/clase.ts
@@ -1,5 +1,17 @@
+/**
+ * Universidad de La Laguna
+ * Asignatura: Desarrollo de Sistemas Informáticos
+ * Séptima práctica de la asignatura DSI
+ * Realizada por: Omar Suárez Doro
+ * Correo: alu0101483474@ull.edu.es
+ */
+
+/**
+ * Represents a CAMBIAR.
+ */
 export class A {
-    constructor() {
-        console.log('Clase A');
-    }
+  constructor() {
+    console.log('Clase A');
+  }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Universidad de La Laguna
+ * Asignatura: Desarrollo de Sistemas Informáticos
+ * Séptima práctica de la asignatura DSI
+ * Realizada por: Omar Suárez Doro
+ * Correo: alu0101483474@ull.edu.es
+ */
+
 import { A } from './clase.js'
 
 let a = new A;
